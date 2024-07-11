a148f30f879f6e50732334b1c811b9c0ae6fd877
Thomas Ströder <thomas.stroeder@email.de>
2024-07-11 13:21:58 +0100

    simplified EventTest

--- src/test/java/battleship/model/EventTest.java
+++ src/test/java/battleship/model/EventTest.java
@@ -1,7 +1,5 @@
 package battleship.model;
 
-import java.util.concurrent.*;
-
 import org.testng.*;
 import org.testng.annotations.*;
 
@@ -15,49 +13,81 @@ public class EventTest {
         try {
             PLACEMENT =
                 new ShipPlacement(ShipType.BATTLESHIP, new Coordinate(0, 0), Direction.SOUTH, Player.FIRST);
-            TimeUnit.MILLISECONDS.sleep(1);
+            Thread.sleep(1);
             PLACEMENT2 =
                 new ShipPlacement(ShipType.BATTLESHIP, new Coordinate(0, 0), Direction.SOUTH, Player.FIRST);
-            TimeUnit.MILLISECONDS.sleep(1);
+            Thread.sleep(1);
             SHOT = new Shot(new Coordinate(0, 0), Player.SECOND);
         } catch (final InterruptedException e) {
             throw new IllegalStateException(e);
         }
     }
 
-    @Test
-    public void compareToTest() {
-        Assert.assertEquals(EventTest.PLACEMENT.compareTo(EventTest.PLACEMENT2), -1);
-        Assert.assertEquals(EventTest.PLACEMENT2.compareTo(EventTest.PLACEMENT), 1);
-        Assert.assertEquals(EventTest.PLACEMENT.compareTo(EventTest.SHOT), -1);
-        Assert.assertEquals(EventTest.SHOT.compareTo(EventTest.PLACEMENT2), 1);
-        Assert.assertEquals(EventTest.PLACEMENT.compareTo(EventTest.PLACEMENT), 0);
-        Assert.assertEquals(EventTest.SHOT.compareTo(EventTest.SHOT), 0);
+    @DataProvider
+    public Object[][] compareToData() {
+        return new Object[][] {
+            {EventTest.PLACEMENT, EventTest.PLACEMENT2, -1},
+            {EventTest.PLACEMENT2, EventTest.PLACEMENT, 1},
+            {EventTest.PLACEMENT, EventTest.SHOT, -1},
+            {EventTest.SHOT, EventTest.PLACEMENT2, 1},
+            {EventTest.PLACEMENT, EventTest.PLACEMENT, 0},
+            {EventTest.SHOT, EventTest.SHOT, 0}
+        };
     }
 
-    @Test
-    public void equalsTest() {
-        Assert.assertEquals(EventTest.PLACEMENT, EventTest.PLACEMENT);
-        Assert.assertEquals(EventTest.SHOT, EventTest.SHOT);
-        Assert.assertNotEquals(EventTest.PLACEMENT, EventTest.SHOT);
-        Assert.assertNotEquals(EventTest.PLACEMENT, EventTest.PLACEMENT2);
+    @Test(dataProvider="compareToData")
+    public void compareToTest(final Event first, final Event second, final int expected) {
+        Assert.assertEquals(first.compareTo(second), expected);
     }
 
-    @Test
-    public void isShipPlacementEventTest() {
-        Assert.assertTrue(EventTest.PLACEMENT.isShipPlacementEvent(Player.FIRST));
-        Assert.assertTrue(EventTest.PLACEMENT2.isShipPlacementEvent(Player.FIRST));
-        Assert.assertFalse(EventTest.PLACEMENT.isShipPlacementEvent(Player.SECOND));
-        Assert.assertFalse(EventTest.SHOT.isShipPlacementEvent(Player.FIRST));
-        Assert.assertFalse(EventTest.SHOT.isShipPlacementEvent(Player.SECOND));
+    @DataProvider
+    public Object[][] equalsData() {
+        return new Object[][] {
+            {EventTest.PLACEMENT, EventTest.PLACEMENT, true},
+            {EventTest.SHOT, EventTest.SHOT, true},
+            {EventTest.PLACEMENT, EventTest.SHOT, false},
+            {EventTest.PLACEMENT, EventTest.PLACEMENT2, false}
+        };
     }
 
-    @Test
-    public void isShotEventTest() {
-        Assert.assertTrue(EventTest.SHOT.isShotEvent(Player.SECOND));
-        Assert.assertFalse(EventTest.SHOT.isShotEvent(Player.FIRST));
-        Assert.assertFalse(EventTest.PLACEMENT.isShotEvent(Player.FIRST));
-        Assert.assertFalse(EventTest.PLACEMENT.isShotEvent(Player.SECOND));
+    @Test(dataProvider="equalsData")
+    public void equalsTest(final Event first, final Event second, final boolean expected) {
+        if (expected) {
+            Assert.assertEquals(first, second);
+        } else {
+            Assert.assertNotEquals(first, second);
+        }
+    }
+
+    @DataProvider
+    public Object[][] isShipPlacementEventData() {
+        return new Object[][] {
+            {EventTest.PLACEMENT, Player.FIRST, true},
+            {EventTest.PLACEMENT2, Player.FIRST, true},
+            {EventTest.PLACEMENT, Player.SECOND, false},
+            {EventTest.SHOT, Player.FIRST, false},
+            {EventTest.SHOT, Player.SECOND, false}
+        };
+    }
+
+    @Test(dataProvider="isShipPlacementEventData")
+    public void isShipPlacementEventTest(final Event event, final Player player, final boolean expected) {
+        Assert.assertEquals(event.isShipPlacementEvent(player), expected);
+    }
+
+    @DataProvider
+    public Object[][] isShotEventData() {
+        return new Object[][] {
+            {EventTest.SHOT, Player.SECOND, true},
+            {EventTest.SHOT, Player.FIRST, false},
+            {EventTest.PLACEMENT, Player.FIRST, false},
+            {EventTest.PLACEMENT, Player.SECOND, false}
+        };
+    }
+
+    @Test(dataProvider="isShotEventData")
+    public void isShotEventTest(final Event event, final Player player, final boolean expected) {
+        Assert.assertEquals(event.isShotEvent(player), expected);
     }
 
 }
